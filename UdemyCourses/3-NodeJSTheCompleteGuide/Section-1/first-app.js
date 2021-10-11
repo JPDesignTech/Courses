366c852c67beb29eb32211c94b537e6b649bb685
Jean <jpere547@fiu.edu>
2021-10-11 13:30:11 -0400

    Merging in #9 .

--- UdemyCourses/3-NodeJSTheCompleteGuide/Section-1/first-app.js
+++
@@ -1,4 +0,0 @@
-console.log('NodeJs');
-
-const fs = require('fs'); // File System Inport
-fs.writeFileSync('hellow.txt', 'Hello There! General Konobi...');
--- UdemyCourses/3-NodeJSTheCompleteGuide/Section-3/app.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-
-const routes = require('./routes');
-
-const ourServer = http.createServer(routes);
-
-ourServer.listen(3000);
